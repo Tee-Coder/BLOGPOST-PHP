e303a8adcd942a85269f906de7af4888aca356f4
Toyin <oluwatoyinogundare@gmail.com>
2020-04-30 23:21:05 -0600

    navigation page created

---
+++ includes/navigation.php
@@ -0,0 +1,12 @@
+<nav>
+    <h2>Website Navigation</h2>
+    <ul>
+        <li>
+            <a href="./index.php">Home</a>
+        </li>
+        <li>
+            <a href="./blog-post.php">Blogpost Page</a>
+        </li>
+        
+    </ul>
+</nav>
